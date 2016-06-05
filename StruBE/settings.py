34b49219a9d44b3f62cf75d8fcb93f2ddadefc34
Arnaud Hungler <arnaud.hungler@kaust.edu.sa>
2016-06-05 09:59:37 +0300

    Use APP_DIRS instead of loaders to charge templates

--- StruBE/settings.py
+++ StruBE/settings.py
@@ -135,23 +135,18 @@ MIDDLEWARE_CLASSES = [
 log.debug("Middlewares : " + str(MIDDLEWARE_CLASSES))
 
 # Templates configuration
-strube_dir = os.path.join(BASE_DIR, "StruBE")
-template_dirs = [os.path.join(strube_dir, "templates")]
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': template_dirs,
+        'DIRS': [],
+        'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-            ],
-            'loaders': [
-                'django.template.loaders.filesystem.Loader',
-                'django.template.loaders.app_directories.Loader',
             ],
         },
     },
