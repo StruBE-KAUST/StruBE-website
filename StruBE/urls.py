22b277c974d8089905a4d7749eaec22168bdc4a9
Dunatotatos <contact@dunatotatos.com>
2017-10-10 17:07:55 +0300

    Add robots.txt file

--- StruBE/urls.py
+++ StruBE/urls.py
@@ -44,6 +44,10 @@ urlpatterns = [
     url(r'^instruments/',
         TemplateView.as_view(template_name="instruments.html"),
         name='instruments'),
+    url(r'^robots\.txt$',
+        TemplateView.as_view(
+            template_name="robots.txt",
+            content_type='text/plain')),
 ]
 
 if settings.DEBUG:
